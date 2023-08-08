48ddd46ee87955ca7461ba183ff62d8bfd0551f0
Nikita Trofimov <n.v.trofimov@tinkoff.ru>
2023-08-09 01:43:02 +0300

    tag user by username

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from decouple import config
-from telegram import Update
+from telegram import Update 
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 import logging
 from calculate_time_until import calculate_time_until
+from telegram.constants import ParseMode
 
 BOT_TOKEN= config('BOT_TOKEN')
 logging.basicConfig(
@@ -25,25 +26,30 @@ def remove_job_if_exists(name: str, context: ContextTypes.DEFAULT_TYPE) -> bool:
     return True
 
 async def bet_reminder_text(context: ContextTypes.DEFAULT_TYPE) -> None:
+    job = context.job
     days, hours, minutes, seconds = calculate_time_until(2023, 9, 1, 21, 00, 00)
     await context.bot.send_message(chat_id='262093856', text=f"""
-Напоминаю про спор со страшим псом. \n
-До окончания спора осталось {days} дней {hours} часов {minutes} минут {seconds} секунд \n
-Надеюсь ты сегодня сделал все, чтобы приблизить себя к победе
+@{job.data} Напоминаю про спор со страшим псом\. \n
+Для победы тебе нужно набрать массу к назначенному времени, чтобы сделать 20 чистых отжиманий иначе касарь должен псу или маме 
+До окончания спора осталось: \n  <b>{days} дней {hours} часов {minutes} минут {seconds} секунд</b> \n
+Надеюсь ты сегодня сделал все, чтобы приблизить себя к победе, потому что второй пес получает такие же мотивационные сообщения каждый день и стремиться к победе
                                    """)
 
 async def set_bet_reminder(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     chat_id = update.effective_message.chat_id
+    user = update.message.from_user
     try:
         job_removed = remove_job_if_exists(str(chat_id), context)
-        context.job_queue.run_repeating(bet_reminder_text, interval=5, first=2)
+        context.job_queue.run_repeating(bet_reminder_text, interval=5, first=1, data=user.username)
     except (IndexError, ValueError):
         await update.effective_message.reply_text("Run /start again")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text='''
-Здарова Пес! Ты попал в логово крутых псов. \n
+Здарова Пес! Если ты оказался в этом чате, значит ты истинный пес, можешь гордиться этим. \n
 Тут будут всякие напоминания о каких-то событиях, спорах, умные мысли и главное напоминание о том, что ты пес
+Если у тебя будут предложения по улучшению этого бота, пиши главному псу. Возможно ты хочешь чаще получать сообщения о том, что ты пес \n\n
+А пока лови первую напоминалку
                                    ''')
     await set_bet_reminder(update, context)
 
